ee3881d8094dab643eab3accc8fce878dc3468f3
Aaron Turner <aaron@aaronthedev.com>
2020-05-22 17:33:50 -0700

    Made a closure test

---
+++ tests/compiler/closure-common-js-patterns.ts
@@ -0,0 +1,8 @@
+// Common use cases / concepts for closures, as covered in articles like:
+// https://medium.com/@dis_is_patrick/practical-uses-for-closures-c65640ae7304
+// https://stackoverflow.com/questions/2728278/what-is-a-practical-use-for-a-closure-in-javascript
+// https://softwareengineering.stackexchange.com/questions/285941/why-would-a-program-use-a-closure
+// https://medium.com/@prashantramnyc/what-is-an-iife-in-javascript-24baf0febf08
+
+
+assert(true == true);
